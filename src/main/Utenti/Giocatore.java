499384bdc2ff38feb23a5b53750b47f24dd2d740
edoardocasali <edoardocasali8@gmail.com>
2024-11-11 11:19:19 +0100

    feat: aggiunto classi utente, giocatore, scrittore e visitatore

---
+++ src/main/Utenti/Giocatore.java
@@ -0,0 +1,27 @@
+public class Giocatore implements Utente {
+    private String nome, password; // incapsulation
+
+    public Giocatore(String nome, String password) {
+        this.nome = nome;
+        this.password = password;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // lista getStorieGiocate
+    // set storiaGiocata
+    // lista getDecisioniPrese
+    // set decisionePresa
+    // set rispostaIndovinello
+    // lista indovinello risposti
+}
---
+++ src/main/Utenti/Utente.java
@@ -0,0 +1,6 @@
+interface Utente
+{
+    String getNome();
+    void setNome(String nome);
+    void setPassword(String password);
+}
