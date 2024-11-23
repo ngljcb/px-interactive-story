71b2ebecb268f35eea0ed4574c39333a209d8683
Jake / <75119871+ngljcb@users.noreply.github.com>
2024-11-23 11:01:28 +0100

    update auth
    Co-Authored-By: edoardocasali <65403151+edoardocasali@users.noreply.github.com>
    Co-Authored-By: Francesco Bondi <126830952+fra-bondi@users.noreply.github.com>

---
+++ src/main/java/com/sweng/InteractiveStory/dto/UserRequest.java
@@ -0,0 +1,42 @@
+package com.sweng.InteractiveStory.dto;
+
+public class UserRequest {
+    private String uid;
+    private String email;
+    private String username;
+    private boolean storyAdmin;
+
+    // Getters e Setters
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isStoryAdmin() {
+        return storyAdmin;
+    }
+
+    public void setStoryAdmin(boolean storyAdmin) {
+        this.storyAdmin = storyAdmin;
+    }
+}
+
