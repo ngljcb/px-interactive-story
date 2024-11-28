dac08054f6ace0dd65c1ed9c7f2306a1b2559713
Jake / <75119871+ngljcb@users.noreply.github.com>
2024-11-28 16:39:25 +0100

    delete file: scelta.java
    Co-Authored-By: edoardocasali <65403151+edoardocasali@users.noreply.github.com>
    Co-Authored-By: Francesco Bondi <126830952+fra-bondi@users.noreply.github.com>

--- src/main/java/com/sweng/InteractiveStory/entity/decisione/Scelta.java
+++
@@ -1,5 +0,0 @@
-package com.sweng.InteractiveStory.entity.decisione;
-import com.sweng.InteractiveStory.entity.Scenario;
-public interface Scelta {
-    Scenario goToScenario(Scenario scenario1, Scenario scenario2);
-}
