16651f845ebb5a969f708e785c166bfe955b6c29
Jake / <75119871+ngljcb@users.noreply.github.com>
2024-12-03 20:19:35 +0100

    fix safeCastToMap

--- application/InteractiveStory/src/main/java/com/sweng/InteractiveStory/config/SecurityConfig.java
+++ application/InteractiveStory/src/main/java/com/sweng/InteractiveStory/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.sweng.InteractiveStory.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
