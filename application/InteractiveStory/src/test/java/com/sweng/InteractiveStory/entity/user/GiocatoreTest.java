45aecdcf4f7374e88fc13f44fa9a95421402f5c4
edoardocasali <edoardocasali8@gmail.com>
2024-12-03 18:22:23 +0100

    feat: update InventarioTest.java

--- application/InteractiveStory/src/test/java/com/sweng/InteractiveStory/entity/user/GiocatoreTest.java
+++ application/InteractiveStory/src/test/java/com/sweng/InteractiveStory/entity/user/GiocatoreTest.java
@@ -1,9 +1,6 @@
 package com.sweng.InteractiveStory.entity.user;
 
 import org.junit.jupiter.api.Test;
-
-
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class GiocatoreTest {
@@ -11,7 +8,6 @@ class GiocatoreTest {
     @Test
     void testCostruttoreConParametri() {
         Giocatore giocatore = new Giocatore("1", "Mario Rossi", "mario.rossi@example.com");
-
         assertEquals("1", giocatore.getUid());
         assertEquals("Mario Rossi", giocatore.getNome());
         assertEquals("mario.rossi@example.com", giocatore.getEmail());
@@ -20,7 +16,6 @@ class GiocatoreTest {
     @Test
     void testCostruttoreSenzaParametri() {
         Giocatore giocatore = new Giocatore();
-
         assertNull(giocatore.getUid());
         assertNull(giocatore.getNome());
         assertNull(giocatore.getEmail());
@@ -30,7 +25,6 @@ class GiocatoreTest {
     void testSetterGetterUid() {
         Giocatore giocatore = new Giocatore();
         giocatore.setUid("2");
-
         assertEquals("2", giocatore.getUid());
     }
 
@@ -38,7 +32,6 @@ class GiocatoreTest {
     void testSetterGetterNome() {
         Giocatore giocatore = new Giocatore();
         giocatore.setNome("Luigi Verdi");
-
         assertEquals("Luigi Verdi", giocatore.getNome());
     }
 
@@ -46,8 +39,6 @@ class GiocatoreTest {
     void testSetterGetterEmail() {
         Giocatore giocatore = new Giocatore();
         giocatore.setEmail("luigi.verdi@example.com");
-
         assertEquals("luigi.verdi@example.com", giocatore.getEmail());
     }
 }
-
